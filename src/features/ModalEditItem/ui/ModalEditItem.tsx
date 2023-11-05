ed38aeac033e56c04f531b2f258cbf3e4c5b7ecf
danilkk <ezzfvkoko@gmail.com>
2023-11-05 18:43:21 +0300

    fix: change name into empty string

--- src/features/ModalEditItem/ui/ModalEditItem.tsx
+++ src/features/ModalEditItem/ui/ModalEditItem.tsx
@@ -1,68 +1,73 @@
 /* VENDOR */
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, {useState} from "react";
+import {useLocation} from "react-router-dom";
 
 /* APPLICATION */
-import { ModalEditItemProps } from "../model/props";
+import {ModalEditItemProps} from "../model/props";
 import {Modal, ModalFooter, ModalHeader, ModalInput, ModalRow, ModalTextarea} from "@/shared/ui/Modal";
 import {categoriesUpdated} from "@/entities/Category";
 import {tasksUpdated} from "@/entities/Task";
 import {useAppDispatch} from "@/shared/hooks/hooks";
+import {Item} from "@/shared/model/item";
 
 export const ModalEditItem: React.FC<ModalEditItemProps> = ({
-  item,
-  active,
-  setActive,
-}) => {
-  const dispatch = useAppDispatch(),
-    { pathname } = useLocation(),
-    isCategories = pathname.includes("categories"),
-    [name, setName] = useState(item.name),
-    [selected, setSelected] = useState(item.category || ""),
-    [description, setDescription] = useState(item.description);
+                                                                item,
+                                                                active,
+                                                                setActive,
+                                                            }) => {
+    const dispatch = useAppDispatch(),
+        {pathname} = useLocation(),
+        isCategories = pathname.includes("categories"),
+        [name, setName] = useState<Item["name"]>(item.name),
+        [selected, setSelected] = useState<Required<Pick<Item, "category">>["category"]>(item.category && item.category || ""),
+        [description, setDescription] = useState<Item["description"]>(item.description);
 
     const handleSubmit = () => {
-        dispatch(
-            isCategories
-                ? categoriesUpdated({ id: item.id, name, description })
-                : tasksUpdated({
-                    id: item.id,
-                    name,
-                    description,
-                    category: selected,
-                })
-        );
+        if (isCategories && name)
+            dispatch(
+                categoriesUpdated({id: item.id, name, description})
+            );
+        else if (name)
+            dispatch(tasksUpdated({
+                id: item.id,
+                name,
+                description,
+                category: selected || undefined,
+            }))
+        else
+
+            return;
         setActive(false);
     };
 
     return (
-    <Modal active={active} setActive={setActive}>
-      <ModalHeader
-        setActive={setActive}
-        title={
-          isCategories ? "Редактирование категории" : "Редактирование задачи"
-        }
-      />
-      {isCategories ? (
-        <ModalInput name={name} setName={setName} size="large" />
-      ) : (
-        <ModalRow
-          name={name}
-          setName={setName}
-          selected={selected}
-          setSelected={setSelected}
-        />
-      )}
-      <ModalTextarea
-        description={description}
-        setDescription={setDescription}
-      />
-      <ModalFooter
-        setActive={setActive}
-        submitBtnText="Сохранить"
-        size="large"
-        onSubmit={handleSubmit}
-      />
-    </Modal>
-  );
+        <Modal active={active} setActive={setActive}>
+            <ModalHeader
+                setActive={setActive}
+                title={
+                    isCategories ? "Редактирование категории" : "Редактирование задачи"
+                }
+            />
+            {isCategories ? (
+                <ModalInput name={name} setName={setName} size="large"/>
+            ) : (
+                <ModalRow
+                    name={name}
+                    setName={setName}
+                    selected={selected}
+                    setSelected={setSelected}
+                />
+            )}
+            <ModalTextarea
+                description={description}
+                setDescription={setDescription}
+            />
+            <ModalFooter
+                setActive={setActive}
+                submitBtnText="Сохранить"
+                size="large"
+                onSubmit={handleSubmit}
+            />
+        </Modal>
+    );
 };
